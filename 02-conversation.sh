67c1ba4172571a9907d6fcbfbb2c123b9b85f1a3
Aswini306 <ashupuri5@gmail.com>
2024-09-17 22:41:01 +0530

    this is a conversation

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "ramesh :: hi suresh,,how r u?"
+echo "suresh :: hi ramesh,,i am good,how r u?"
+echo "ramesh :: iam good ..what's going on?"
+echo "suresh :: iam learning devops..it is amazing"
