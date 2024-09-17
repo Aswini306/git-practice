ebabbbb54092542d479ea1c9f0dad45571b80f88
Aswini306 <ashupuri5@gmail.com>
2024-09-17 23:11:43 +0530

    this is a variable script

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-$person1 = ramesh
-$person2= suresh
+PERSON1 = ramesh
+PERSON2 = suresh
 
 
-echo "$person1 :: hi person2,,how r u?"
-echo "$person2 :: hi $person1,,i am good,how r u?"
-echo "$person1 :: iam good ..what's going on?"
-echo "$person2 :: iam learning devops..it is amazing"
+echo "$PERSON1 :: hi $PERSON2,,how r u?"
+echo "$PERSON2 :: hi $PERSON1,,i am good,how r u?"
+echo "$PERSON1 :: iam good ..what's going on?"
+echo "$PERSON2 :: iam learning devops..it is amazing"
