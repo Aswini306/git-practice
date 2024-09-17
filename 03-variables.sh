def5a27ce01fb1946a6f1380afc3c03bcf24fe0f
Aswini306 <ashupuri5@gmail.com>
2024-09-17 23:04:36 +0530

    this is a variable script

---
+++ 03-variables.sh
@@ -0,0 +1,8 @@
+person1 = ramesh
+person2= suresh
+
+
+echo "$person1 :: hi ,,how r u?"
+echo "$person2 :: hi $person1,,i am good,how r u?"
+echo "$person1 :: iam good ..what's going on?"
+echo "$person2 :: iam learning devops..it is amazing"
