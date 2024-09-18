4ecc7eb4ca56cbefa72ae7dd57fe1ed5cff39188
Aswini306 <ashupuri5@gmail.com>
2024-09-18 21:08:35 +0530

    this is a variable script

---
+++ 04-variables.sh
@@ -0,0 +1,9 @@
+#!\bin\bash
+PERSON1=$1
+PERSON2=$2
+
+
+echo "$PERSON1 :: hi $PERSON2,,how r u?"
+echo "$PERSON2 :: hi $PERSON1,,i am good,how r u?"
+echo "$PERSON1 :: iam good ..what's going on?"
+echo "$PERSON2 :: iam learning devops..it is amazing"
