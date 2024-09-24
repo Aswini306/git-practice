a76d07431cc0c62dac71e63cbba9284df50a97fc
Aswini306 <ashupuri5@gmail.com>
2024-09-24 11:44:20 +0530

    if and else

--- 10-conditions.sh
+++ 10-conditions.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-num1= $1
+num1=$1
 
-if[ $num1-gt 89 ]
+if[ $num1 -gt 89 ]
 then
     echo " given number $num1 is greater than 89"
-fi
 else
     echo " given number $num1 is lesser than 89"
 fi
