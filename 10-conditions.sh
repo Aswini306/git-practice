db72e22c02da4887df04f64e488371b57f9b8776
Aswini306 <ashupuri5@gmail.com>
2024-09-24 11:29:41 +0530

    if and else

--- 10-conditions.sh
+++ 10-conditions.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 num1=$1
-num2=$2
 
-if(num1>num2)
-{
-    echo "$num1 is greater than $num2"
-}
+if[ $num -gt 89]
+then
+    echo " given number $num1 is greater than 89"
 else
-{
-    echo "$num1 is lesser than $num2"
-}
+     echo " given number $num1 is lesser than 89"
+fi
+
+
