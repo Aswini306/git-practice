51f404ba2e4483e86eb66d2d7a96eae5b1b144ea
Aswini306 <ashupuri5@gmail.com>
2024-11-16 17:03:05 +0530

    this is colours program

--- 12-colour.sh
+++ 12-colour.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 USERID=$(id -u)
-R=$ "\e[31m"
-G=$ "\e[32m"
-N=$ "\e[0m"
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
 
 CHECK_ROOT(){
     if [ $USERID -ne 0 ]
@@ -16,10 +16,10 @@ CHECK_ROOT(){
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-        echo-e "$2 is... $R FAILED $N "
+        echo -e "$2 is... $R FAILED $N "
         exit 1
     else
-        echo-e "$2 is... $G SUCCESS $N"
+        echo -e "$2 is... $G SUCCESS $N"
     fi
 }
 
